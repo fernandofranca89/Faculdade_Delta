eb8842ea0f64fe33cf5956c4e529eeddd1f79936
fernandofranca89 <63021925+fernandofranca89@users.noreply.github.com>
2020-04-03 12:41:53 -0300

    Aula 3
    Código referente a aula 3 Extremamente Básico

---
+++ Extremamente Básico.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+     
+     
+     int A;
+     int B;
+     
+      Scanner sc = new Scanner(System.in);
+     
+     System.out.println("X1");
+     A = sc.nextInt();
+     
+     System.out.println("X2");
+     B = sc.nextInt();
+     
+     
+     
+     System.out.println("x = " + (A + B));
+    }
+}
