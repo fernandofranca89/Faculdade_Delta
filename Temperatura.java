8836e875e49cf3fe4119c4ed6c93942a7040edf1
Fernando França <63021925+fernandofranca89@users.noreply.github.com>
2020-10-15 10:37:30 -0300

    Update Temperatura.java

--- Temperatura.java
+++ Temperatura.java
@@ -12,21 +12,21 @@ public class Main {
             System.out.println("digite a opereção desejada");
             System.out.println("digite 1 para converter a temperatura em ºc, 2 para ºF e 3 para Kelvin");
              x = sc.nextInt();{
-                if(x==1);{
+                if(x==1){
                     System.out.println("digite a temperatura em ºC");
                     c = sc.nextDouble();
                     f = ((c*9/5)+31);
                     k =(c+273.15);
                     System.out.println("ºF " + f);
                     System.out.println("Kelvin " + k);
-                }else if (x==2);{
+                }else if (x==2){
                     System.out.println("digite a temperatura em ºF");
                     f = sc.nextDouble();
                     c = ((f-32)*5/9);
                     k = ((f-32)*5/9+273.15);
                     System.out.println("ºC "+ c);
                     System.out.println("Kelvin "+ k);
-                }else if (x==3);{
+                }else if (x==3){
                     System.out.println("digite a temperatura em Kelvin");
                     k = sc.nextDouble();
                     c = (k-273.15);
